6c78adba666450e5d443815fe7d3a60c790d036c
Ferdinand Schober <ferdinandschober20@gmail.com>
2026-01-30 00:01:08 +0100

    impl wifi credential store

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod nvs;
+
 fn main() {
     // It is necessary to call this function once. Otherwise, some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -5,6 +7,14 @@ fn main() {
 
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
+
+    match nvs::load_wifi_credentials() {
+        Ok(cred) => match cred {
+            Some((ssid, pw)) => log::info!("loaded credentials: ssid={ssid}, pw={pw}"),
+            None => todo!(),
+        },
+        Err(e) => log::warn!("failed to load wifi credentials: {e}"),
+    }
 
     log::info!("Hello, world!");
 }
---
+++ src/nvs.rs
@@ -0,0 +1,35 @@
+use esp_idf_svc::nvs::{EspDefaultNvsPartition, EspNvs, NvsPartitionId};
+
+const WIFI_NS: &'static str = "wifi";
+const SSID_KEY: &'static str = "ssid";
+const PW_KEY: &'static str = "password";
+
+pub(crate) fn save_wifi_credentials(ssid: &str, password: &str) -> anyhow::Result<()> {
+    let nvs_part = EspDefaultNvsPartition::take().expect("failed to load nvs partition");
+    let mut namespace = EspNvs::new(nvs_part, WIFI_NS, true)?;
+    namespace.set_raw(SSID_KEY, ssid.as_bytes())?;
+    namespace.set_raw(PW_KEY, password.as_bytes())?;
+    Ok(())
+}
+
+pub(crate) fn load_wifi_credentials() -> anyhow::Result<Option<(String, String)>> {
+    let nvs_part = EspDefaultNvsPartition::take().expect("failed to load nvs partition");
+    let namespace = EspNvs::new(nvs_part, WIFI_NS, false)?;
+    let ssid = read_to_string(&namespace, SSID_KEY)?;
+    let password = read_to_string(&namespace, PW_KEY)?;
+    match (ssid, password) {
+        (Some(ssid), Some(password)) => Ok(Some((ssid, password))),
+        _ => Ok(None),
+    }
+}
+
+fn read_to_string<T: NvsPartitionId>(
+    namespace: &EspNvs<T>,
+    key: &str,
+) -> anyhow::Result<Option<String>> {
+    let mut buf = [0u8; 128];
+    match namespace.get_raw(key, &mut buf)? {
+        Some(val) => Ok(Some(str::from_utf8(val)?.to_string())),
+        None => Ok(None),
+    }
+}
